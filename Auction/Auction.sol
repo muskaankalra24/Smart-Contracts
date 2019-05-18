0c95f6aa4c1d45389b098b31a820d43ddde354b9
Yogesh <17ucs188@lnmiit.ac.in>
2019-05-18 13:06:28 +0530

    Update withdraw function

--- Auction/Auction.sol
+++ Auction/Auction.sol
@@ -77,21 +77,20 @@ contract SimpleAuction {
     }
 
     /// Withdraw a bid that was overbid.
-    function withdraw() public returns (bool) {
+    function withdraw() public {
         uint amount = pendingReturns[msg.sender];
-        if (amount > 0) {
+        bool x;
+        require(msg.sender != highestBidder,"You are the highest bidder:Cannot withdraw");
+        require(amount > 0,"You have nothing to withdraw");
             // It is important to set this to zero because the recipient
             // can call this function again as part of the receiving call
             // before `send` returns.
             pendingReturns[msg.sender] = 0;
 
-            if (!msg.sender.send(amount)) {
-                // No need to call throw here, just reset the amount owing
-                pendingReturns[msg.sender] = amount;
-                return false;
-            }
-        }
-        return true;
+            if (x = !msg.sender.send(amount)) 
+               pendingReturns[msg.sender] = amount;
+            require(x==false,"Withdraw failed");   
+            
     }
 
     /// End the auction and send the highest bid
